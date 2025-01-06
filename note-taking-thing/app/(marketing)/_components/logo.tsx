a2a48b39fffc96dbb8ebbcf763294402fae80ad3
HarpinderSingh1999 <tui57086@temple.edu>
2025-01-06 11:33:39 -0500

    logo isn;t appearing at bottom left correctly

---
+++ note-taking-thing/app/(marketing)/_components/logo.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ['400', '600']
+});
+
+export const Logo = () => {
+    return (
+        <div className='hidden md:flex items-center gap-x-2'>
+            <Image
+            src='/einstein.png'
+            height='40'
+            width='40'
+            alt="Logo"
+            ></Image>
+            <p className={cn("font-semibold", font.className)}>my notebook thingy</p>
+
+        </div>
+    )
+}
--- note-taking-thing/app/(marketing)/page.tsx
+++ note-taking-thing/app/(marketing)/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
+import { Footer } from "./_components/footer";
+
 
 const MarketPage = () => {
   return (
@@ -7,7 +10,10 @@ const MarketPage = () => {
       <div className="flex flex-col items-center justify-center 
       md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading></Heading>
-
+        <Heroes></Heroes>
+        <div>
+          <Footer></Footer>
+        </div>
       </div>
     </div>
   );  
