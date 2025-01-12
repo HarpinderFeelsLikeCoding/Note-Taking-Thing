d53d1631c64f5827ded3bf504b7be2f392508b71
HarpinderSingh1999 <tui57086@temple.edu>
2025-01-12 17:02:21 -0500

    sidebar functiionality started

--- note-taking-thing/app/(marketing)/_components/navbar.tsx
+++ note-taking-thing/app/(marketing)/_components/navbar.tsx
@@ -42,7 +42,7 @@ export const Navbar = () => {
             {isAuthenticated && !isLoading &&(
                 <>
                 <Button variant = 'ghost' size='sm' asChild>
-                    <Link href='/documents'>
+                    <Link href = '/documents'>
                     Open your Notebook
                     </Link>
                 </Button>
