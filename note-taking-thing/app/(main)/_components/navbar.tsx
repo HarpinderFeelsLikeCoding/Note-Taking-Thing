63d70048bb4518e44e7e9a33ce10449937a9f393
HarpinderSingh1999 <tui57086@temple.edu>
2025-02-14 17:04:54 -0500

    woo wee almost done

--- note-taking-thing/app/(main)/_components/navbar.tsx
+++ note-taking-thing/app/(main)/_components/navbar.tsx
@@ -5,6 +5,8 @@ import { useQuery } from 'convex/react';
 import { MenuIcon } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { Title } from './title';
+import { Banner } from './banner';
+import { Menu } from './menu';
 interface NavbarProps {
     isCollapsed: boolean;
     onResetWidth: () => void;
@@ -20,11 +22,18 @@ export const Navbar = ({
         documentId: params.documentId as Id<'documents'>,
     });
 
-    if (document === undefined){
-        return <p>Loading...</p>
+    if (document === undefined) {
+        return (
+            <nav className = 'bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between'>
+                <Title.Skeleton/>
+                <div className = 'flex items-center gap-x-2'>
+                    <Menu.Skeleton />
+                </div>
+            </nav>
+        )
     }
 
-    if (document === null){
+    if (document === null) {
         return null;
     }
 
@@ -39,9 +48,14 @@ export const Navbar = ({
                 )}
                 <div className='flex items-center justify-between w-full'>
                     <Title initialData={document}></Title>
-
+                    <div className='flex items-center gap-x-2'>
+                        <Menu documentId={document._id}/>
+                    </div>
                 </div>
             </nav>
+            {document.isArchived && (
+                <Banner documentId={document._id}/>
+            )}
         </>
     )
 }
--- note-taking-thing/app/(main)/_components/title.tsx
+++ note-taking-thing/app/(main)/_components/title.tsx
@@ -3,9 +3,10 @@
 import { Doc } from "@/convex/_generated/dataModel";
 import { useMutation } from "convex/react";
 import { api } from '@/convex/_generated/api'
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface TitleProps{
     initialData: Doc<'documents'>;
@@ -14,22 +15,63 @@ interface TitleProps{
 export const Title = ({
     initialData
 }: TitleProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const update = useMutation(api.documents.update);
+    const [title, setTitle] = useState(initialData.title || 'Untitled');
     const [isEditing, setIsEditing] = useState(false);
+
+    const enableInput = () => {
+        setTitle(initialData.title);
+        setIsEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(0,inputRef.current.value.length)
+        },0);
+    }
+
+    const disableInput = () => {
+        setIsEditing(false);
+
+    }
+
+    const onChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTitle(event.target.value);
+        update({
+            id: initialData._id,
+            title: event.target.value || 'Untitled'
+        });
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if (event.key === 'Enter') {
+            disableInput();
+        }
+    }
+
 
     return (
         <div className="lex itmes-center gap-x-1">
             {!!initialData.icon && <p>{initialData.icon}</p>}
             {isEditing ?(
                 <Input
+                ref={inputRef}
+                onClick={enableInput}
+                onBlur={disableInput}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                value={title}
                 className = 'h-7 px-2 focus-visible:ring-transparent'>
                 </Input>
             ): (
                 <Button
-                onClick={()=>{}}
+                onClick={enableInput}
                 variant='ghost'
                 size='sm'
-                className='font-norma h-auto p-1'
+                className='font-normal h-auto p-1'
                 >
                     <span className='truncate'>
                         {initialData?.title}
@@ -39,3 +81,10 @@ export const Title = ({
         </div>
     )
 }
+
+
+Title.Skeleton = function TitleSkeleton () {
+    return (
+        <Skeleton className = 'h-9 w-20 rounded-md'/>
+    );
+};
