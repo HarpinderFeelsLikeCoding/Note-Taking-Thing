3482cbaa6a451613f29862480685859177eb6c8e
HarpinderSingh1999 <tui57086@temple.edu>
2025-02-18 20:02:31 -0500

    peer dependencies were the issue good god, editor part works now just have to make it usable

--- note-taking-thing/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ note-taking-thing/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,58 +1,69 @@
-'use client'
-import { useQuery } from "convex/react";
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import { Cover } from "@/components/cover";
+import { Toolbar } from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { Toolbar } from "@/components/toolbar";
-import { CoverImageModal } from "@/components/models/cover-image-modal";
-import { Cover } from "@/components/cover";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Editor } from "@/components/editor";
+import { useMutation, useQuery } from "convex/react";
 
 interface DocumentIdPageProps {
-    params: {
-        documentId: Id<'documents'>;
-    }
+  params: {
+    documentId: Id<"documents">;
+  };
 }
-const DocumentIdPage = ({
-    params
-}: DocumentIdPageProps) => {
-    const document = useQuery(api.documents.getById,{
-        documentId: params.documentId
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    [],
+  );
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
     });
+  };
 
-    if(document === undefined){
-        return (
-            <div>
-                <Cover.Skeleton/>
-                <div className= 'md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
-                    <div className = 'space-y-4 pl-8 pt-4'>
-                        <Skeleton className = 'h-14 w-[50%]' />
-                        <Skeleton className = 'h-4 w-[80%]' />
-                        <Skeleton className = 'h-4 w-[40%]' />
-                        <Skeleton className = 'h-4 w-[60%]' />
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-    if (document ===null){
-        return <div>Not found</div>
-    }
-
+  if (document === undefined) {
     return (
-        <div className = 'pb-40'>
-            <Cover url={document.coverImage}/>
-            <div className = 'md:max-w-3xl lg:max-w-4xl mx-auto'>
-                <Toolbar initialData={document}/>
-                <Editor
-                onChange= {()=>{}}
-                initialContent={document.content}
-
-                />
-            </div>
+      <div>
+        <Cover.Skeleton />
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-1/2" />
+            <Skeleton className="h-4 w-4/5" />
+            <Skeleton className="h-4 w-2/5" />
+            <Skeleton className="h-4 w-3/5" />
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  }
 
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <Cover url={document.coverImage} />
+      <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
+        <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
+      </div>
+    </div>
+  );
+};
 export default DocumentIdPage;
--- note-taking-thing/components/editor.tsx
+++ note-taking-thing/components/editor.tsx
@@ -1,45 +1,53 @@
-'use client';
+"use client";
 
-import {
-    BlockNoteEditor,
-    PartialBlock
-} from '@blocknote/core';
-
-import {
-    BlockNoteView,
-    useBlockNote
-} from '@blocknote/react';
-
-import '@blocknote/core/style.css';
-import { useTheme } from 'next-themes';
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import { useTheme } from "next-themes";
+import { useEdgeStore } from "@/lib/edgestore";
+import "@blocknote/core/style.css";
+import "@blocknote/mantine/style.css";
 
 interface EditorProps {
-    onChange: (value: string) => void;
-    initialContent?: string;
-    editable?: boolean;
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const res = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return res.url;
+  };
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    uploadFile: handleUpload,
+  });
+
+  const handleEditorChange = () => {
+    onChange(JSON.stringify(editor.document, null, 2));
+  };
+
+  return (
+    <div>
+      <BlockNoteView
+        editable={editable}
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
 };
 
-export const Editor = ({
-    onChange,
-    initialContent,
-    editable
-}: EditorProps) => {
-    const { resolvedTheme }= useTheme();
-    const editor: BlockNoteEditor = useBlockNote({
-        editable,
-        initialContent: initialContent ? JSON.parse(initialContent) as PartialBlock[] : undefined,
-        onEditorContentChange: (editor) => {
-            onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
-        },
-    })
-
-
-    return (
-        <div>
-            <BlockNoteView
-            editor={editor}
-            theme={resolvedTheme ==='dark' ? 'dark' : 'light'}
-            />
-        </div>
-    )
-}
+export default Editor;
